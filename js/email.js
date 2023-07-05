9641617268423d049f4b84c21c5442206aa8070d
alessandrostorchi92 <storchi.alle@gmail.com>
2023-07-05 16:52:47 +0200

    commit impostazione esercizio

---
+++ js/email.js
@@ -0,0 +1,19 @@
+    // L'utente invia la sua email una volta che clicca sulla call to action 
+
+const btnSubmit = document.querySelector(".btn_submit");
+
+btnContinue.addEventListener("click", function () {
+
+    // Dichiarazione delle variabili e dell'array
+
+    const userEmaiIinput = document.querySelector("[name='useremail']").value;
+    const emailList = ["mariorossi@gmail.com", "mattiabianchi@gmail.com", "rosamaria@gmail.com", "francescaverdi@gmail.com", "gianfranconeri@gmai.com", "saraceleste@gmail.com"];
+
+    for (let i = 0; i < emailList.length; i++){
+
+        const currentEmail = emailList[i]
+
+        debugger
+
+    }
+})
